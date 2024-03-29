f0dd02919467d5d7b1465b51ecdec70bcdd57515
Mary Gore <dm101mary@gmail.com>
2024-03-30 01:20:32 +0400

    add thumbs gallery functional

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,11 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
+/* Swiper imports */
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Thumbs } from 'swiper/modules';
+import 'swiper/css';
+/* Componnets */
 import Breadcrumbs from '../UI/Breadcrumbs/Breadcrumbs';
-import { selectProducts } from '../../features/products/productsSlice';
 import Counter from '../../features/counter/Counter';
+/* UI */
 import ButtonStroke from '../UI/Button/ButtonStroke';
 import ButtonFill from '../UI/Button/ButtonFill';
+/* Slices imports */
+import { selectProducts } from '../../features/products/productsSlice';
 import { resetCounter, selectCounterQuantity } from '../../features/counter/counterSlice';
 import { addToCart } from '../../features/cart/cartSlice';
 
@@ -17,7 +24,8 @@ const Product = () => {
     product = products[prodIndex],
     [sizeProduct, setSizeProduct] = useState(''),
     [colorProduct, setColorProduct] = useState(''),
-    dispatch = useDispatch();
+    dispatch = useDispatch(),
+    [thumbs, setThumbs] = useState(null);
 
   const addToCartHandler = () => {
     dispatch(addToCart({ ...product, colorProduct, sizeProduct, prodQuantity: quantity }));
@@ -33,7 +41,55 @@ const Product = () => {
     <div className="product">
       <Breadcrumbs />
       <div className="grid">
-        <div className="prod-gallery"></div>
+        <div className="prod-gallery">
+          <div className="prod-gallery__preview-wrap">
+            {product.detailsImgs && (
+              <Swiper
+                className="preview-slider"
+                direction={'vertical'}
+                onSwiper={setThumbs}
+                spaceBetween={35}
+                slidesPerView={3}
+                grabCursor={true}
+              >
+                {product.detailsImgs.map((src, index) => (
+                  <SwiperSlide key={index}>
+                    <div className="preview-slider__img-wrap">
+                      <img src={`/img/products/${src}`} alt="product img" />
+                    </div>
+                  </SwiperSlide>
+                ))}
+              </Swiper>
+            )}
+          </div>
+          <div className="prod-gallery__main-slider-wrap">
+            <Swiper
+              className="main-slider"
+              direction={'vertical'}
+              modules={[Thumbs]}
+              thumbs={{ swiper: thumbs && !thumbs.destroyed ? thumbs : null }}
+              spaceBetween={35}
+              slidesPerView={1}
+            >
+              {product.detailsImgs &&
+                product.detailsImgs.map((src, index) => (
+                  <SwiperSlide key={index}>
+                    <SwiperSlide className="main-slider__slide">
+                      <img src={`/img/products/${src}`} alt="product img" />
+                    </SwiperSlide>
+                  </SwiperSlide>
+                ))}
+
+              {(!product.detailsImgs || product.detailsImgs.length === 0) && (
+                <SwiperSlide>
+                  <SwiperSlide className="main-slider__slide">
+                    <img src={`/img/products/${product.img}`} alt="product img" />
+                  </SwiperSlide>
+                </SwiperSlide>
+              )}
+            </Swiper>
+          </div>
+        </div>
         <div className="prod-info">
           <h1>{product.title}</h1>
           <div className="product__sizes">

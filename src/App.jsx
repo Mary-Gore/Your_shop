bd307eea4e80a340cdc7ab0c5adee16fd134c662
Mary Gore <dm101mary@gmail.com>
2024-04-04 13:56:15 +0400

    move br router to app js

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import MainLayout from './layouts/MainLayout';
 import Catalog from './components/pages/Catalog/Catalog';
 import Product from './components/Product/Product';
@@ -8,14 +8,16 @@ import FavoriteProducts from './components/FavoriteProducts/FavoriteProducts';
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<MainLayout />}>
-          <Route path="/:audience?/:prod_category?" element={<Catalog />} />
-          <Route path="/:audience?/:prod_category?/:vendor" element={<Product />} />
-          <Route path="cart" element={<Cart />}></Route>
-          <Route path="/favorite_products" element={<FavoriteProducts />} />
-        </Route>
-      </Routes>
+      <BrowserRouter basename={'/Your_shop'}>
+        <Routes>
+          <Route path="/" element={<MainLayout />}>
+            <Route path="/:audience?/:prod_category?" element={<Catalog />} />
+            <Route path="/:audience?/:prod_category?/:vendor" element={<Product />} />
+            <Route path="cart" element={<Cart />}></Route>
+            <Route path="/favorite_products" element={<FavoriteProducts />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,13 +4,10 @@ import { Provider } from 'react-redux';
 import store from './store';
 import './scss/index.scss';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-    <BrowserRouter basename={'/Your_shop'}>
-      <App />
-    </BrowserRouter>
+    <App />
   </Provider>,
 );
